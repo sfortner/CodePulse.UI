00cc10d24cad35bb79a1815a97623adb139b9ddc
Steve Fortner <fortner.steve@gmail.com>
2024-09-30 16:55:02 -0400

    Section 8 - get all blog posts

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -16,4 +16,8 @@ export class BlogPostService {
   createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
     return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogPosts`, data);
   }
+
+  getAllBlogPosts() : Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogPosts`);
+  }
 }
--- src/app/features/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blogpost-list/blogpost-list.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BlogPost } from '../blog-post/models/blog-post-model';
+import { Observable } from 'rxjs';
+import { BlogPostService } from '../blog-post/services/blog-post.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-blogpost-list',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AsyncPipe],
   templateUrl: './blogpost-list.component.html',
   styleUrl: './blogpost-list.component.css'
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+
+  blogPosts$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService) {
+  }
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 
 }
