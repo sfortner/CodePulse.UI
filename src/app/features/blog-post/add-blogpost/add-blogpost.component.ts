92bcd1058b00603c590e0f9d144f22ac7e69c1ec
Steve Fortner <fortner.steve@gmail.com>
2024-09-30 11:07:02 -0400

    Section 7 - Add new BlogPost

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-blogpost',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './add-blogpost.component.html',
   styleUrl: './add-blogpost.component.css'
 })
 export class AddBlogpostComponent {
+  model: AddBlogPost;
+
+  constructor(private blogPostService: BlogPostService, private router: Router) {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date()
+    }
+  }
+
+  onFormSubmit(): void {
+    this.blogPostService.createBlogPost(this.model)
+    .subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('admin/blogposts');
+      }
+    });
+  }
 
 }
---
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post-model';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+    // https://angular.dev/guide/http/setup - import in app.config.ts
+  constructor(private http: HttpClient) { }
+
+  createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/blogPosts`, data);
+  }
+}
